53efeb1fc6c06b5508f64d5f7e7ef2e966dd158e
phlpierani <phl.pierani@gmail.com>
2026-02-07 22:27:34 -0300

    Initial commit - Spring Boot project setup

---
+++ otica/src/main/java/com/example/otica/OticaAcaciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.otica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OticaAcaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OticaAcaciaApplication.class, args);
+	}
+
+}
